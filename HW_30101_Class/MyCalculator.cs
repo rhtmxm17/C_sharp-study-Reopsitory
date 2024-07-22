9056b4882b6d977d70368ec284a91af725d3b68f
JeeSeong <rhtmxm17@gmail.com>
2024-07-22 16:24:14 +0900

    feat/Create MyCalculator.cs
    계산기 클래스 만들기 과제 수행

---
+++ HW_30101_Class/MyCalculator.cs
@@ -0,0 +1,32 @@
+﻿namespace HW_30101_Class
+{
+    static class MyCalculator
+    {
+        public static double Add(double x, double y)
+        {
+            return (x + y); 
+        }
+        public static double Subtract(double x, double y)
+        {
+            return (x - y);
+        }
+        public static double Multiply(double x, double y)
+        {
+            return (x * y);
+        }
+        public static double Divide(double x, double y)
+        {
+            if(y == 0.0)
+            {
+                Console.WriteLine("오류: 0으로 나눌 수 없습니다");
+                return 0.0;
+            }
+            return (x / y);
+        }
+        public static double Squared(double x, double y)
+        {
+            // Math.Pow() https://learn.microsoft.com/ko-kr/dotnet/api/system.math.pow?view=net-8.0
+            return Math.Pow(x, y);
+        }
+    }
+}
