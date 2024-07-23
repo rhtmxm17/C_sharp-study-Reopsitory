7ae7d3baaa2294842d397d282a04f8632ae8395b
JeeSeong <rhtmxm17@gmail.com>
2024-07-23 17:52:16 +0900

    chore/프로젝트 구조 변경(솔루션 통합)
    과제 수행 및 프로젝트 관리의 용이함을 위함

---
+++ HW_30106_abstract/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HW_30106_abstract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
