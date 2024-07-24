6772407625810958e3039990ca47bc6f3d4a569b
JeeSeong <rhtmxm17@gmail.com>
2024-07-24 17:58:54 +0900

    feat/Generic 과제 수행
    Generic 사용해보기
    박싱을 줄이는 제네릭 공부

---
+++ HW_30201_Generic/BoxingStudy.cs
@@ -0,0 +1,57 @@
+﻿namespace HW_30201_Generic
+{
+    public static class BoxingStudy
+    {
+        public interface IDoubleable
+        {
+            public void Doubleself();
+        }
+
+        public struct Point : IDoubleable
+        {
+            public int x;
+            public int y;
+
+            public void Doubleself()
+            {
+                x *= 2;
+                y *= 2;
+            }
+        }
+
+        public static void DoDouble(IDoubleable val)
+        {
+            val.Doubleself();
+        }
+
+        public static void DoDoubleRef(ref IDoubleable val)
+        {
+            val.Doubleself();
+        }
+
+        public static void DoDoubleGeneric<T>(ref T val) where T : IDoubleable
+        {
+            val.Doubleself();
+        }
+
+        public static void BoxingTest()
+        {
+            Point point = new Point() { x = 2, y = 5 };
+
+            Console.WriteLine($"point 초기값: {point.x}, {point.y}"); // 2, 5
+            point.Doubleself();
+            Console.WriteLine($"point.Doubleself() 이후: {point.x}, {point.y}"); // 4, 10
+            DoDouble(point);
+            Console.WriteLine($"DoDouble(point) 이후: {point.x}, {point.y}"); // 4, 10
+
+            // 원본의 값이 변하지 않은 것으로 DoDouble() 내부의 val이 가리키는 위치가
+            // 원본이 아닌 복사본임을 확인 가능
+
+            // 아래 호출은 불가능했음 (struct 참조를 interface 참조로 변환 불가능)
+            // DoDoubleRef(ref point);
+
+            DoDoubleGeneric(ref point);
+            Console.WriteLine($"DoDoubleGeneric(ref point) 이후: {point.x}, {point.y}"); // 4, 10
+        }
+    }
+}
---
+++ HW_30201_Generic/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace HW_30201_Generic
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Inventory<Potion> potionInventory = new(10);
+
+            potionInventory.Add(new Potion("체력 포션"));
+            potionInventory.Add(new Potion("마나 포션"));
+            potionInventory.Add(new Potion("경험치 포션"));
+            potionInventory.Add(new Potion("체력 포션"));
+
+            potionInventory.PrintItemNames();
+
+            potionInventory.Remove();
+            potionInventory.Remove();
+
+            potionInventory.PrintItemNames();
+        }
+    }
+
+    public abstract class Item
+    {
+        public string Name { get; private set; }
+        public Item(string name)
+        {
+            Name = name;
+        }
+    }
+
+    public class Potion : Item
+    {
+        public Potion(string name) : base(name) { }
+    }
+
+    // 제네릭에서 where키워드로 사용 가능한 형식을 제한하는 예시
+    // T는 Item클래스임을 명시해서 Item 및 Item을 상속하는 키워드로 치환해서 사용 가능하다
+    public class Inventory<T> where T : Item
+    {
+        private T?[] list;
+        private int index;
+
+        public Inventory(int size)
+        {
+            list = new T[size];
+        }
+
+        public void Add(T item)
+        {
+            if (index < list.Length)
+            {
+                list[index] = item;
+                index++;
+            }
+        }
+
+        public void Remove()
+        {
+            if (index > 0)
+            {
+                index--;
+                list[index] = null;
+            }
+        }
+
+        public void PrintItemNames()
+        {
+            Console.WriteLine($"아이템 목록: ");
+            foreach (T? item in list)
+            {
+                if (item != null)
+                {
+                    // T가 Item 클래스 또는 Item을 상속한 클래스로 한정했으므로
+                    // Name 필드가 존재하기 때문에 제네릭 내부에서 이와 같이
+                    // 다른 자료형이었다면 사용할 수 없는 명칭이나 사용 방식도 쓸 수 있다
+                    Console.WriteLine(item.Name);
+                }
+            }
+        }
+    }
+}
