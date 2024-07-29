9bbca7d387b20658c8ce12a7aa96b4435c1accec
JeeSeong <rhtmxm17@gmail.com>
2024-07-29 17:54:51 +0900

    feat/스택과 큐 과제 수행
    괄호 검사기와 작업 일정 계산기

---
+++ HW_30303_Queue/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace HW_30303_Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> schadule = new List<int>() { 4, 4, 12, 10, 2, 10 };
+            int workTime = 8;
+
+            List<int> dates = ScheduleDayChecker(schadule, workTime);
+
+            Console.WriteLine($"입력: [{ string.Join(", ", schadule) }]");
+            Console.WriteLine($"출력: [{ string.Join(", ", dates) }]");
+        }
+
+        /// <summary>
+        /// 여러 작업의 작업 소요 시간과 일일 작업 수행 시간을 입력받아 각 작업의 완료 날짜를 계산합니다.
+        /// </summary>
+        /// <param name="requireTimeData">작업 소요 시간 목록입니다. 앞에서부터 작업을 수행하는것으로 보고 계산합니다.</param>
+        /// <param name="dailyWorkTime">일일 작업 수행 시간입니다.</param>
+        /// <returns>각 작업의 완료 날짜입니다. 순서는 requireTimeData로 입력받은 순서와 동일합니다.</returns>
+        public static List<int> ScheduleDayChecker(List<int> requireTimeData, int dailyWorkTime)
+        {
+            if (dailyWorkTime <= 0)
+                return new List<int>();
+
+            List<int> completeDays = new(requireTimeData.Count);
+            Queue<int> workQueue = new Queue<int>(requireTimeData);
+
+            int day = 1;
+            int remainTime = dailyWorkTime;
+
+            // 남은 작업이 있는 동안 반복
+            while (workQueue.Count > 0)
+            {
+                remainTime -= workQueue.Dequeue();
+
+                // 작업시간이 남은 시간보다 크다면 날짜 진행 및 시간 보충
+                while (remainTime < 0) 
+                {
+                    day++;
+                    remainTime += dailyWorkTime;
+                }
+
+                completeDays.Add(day);
+            }
+
+            return completeDays;
+        }
+    }
+}
---
+++ HW_30303_Stack/Program.cs
@@ -0,0 +1,85 @@
+﻿namespace HW_30303_Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("괄호 쌍을 검사할 문자열을 입력 해 주세요.");
+            string input = Console.ReadLine() ?? "";
+
+            if(BracketPairCheck(input))
+            {
+                Console.WriteLine("입력한 문자열은 괄호 쌍이 완성되어 있습니다.");
+            }
+            else
+            {
+                Console.WriteLine("입력한 문자열은 괄호 쌍이 완성되어 있지 않습니다.");
+            }
+
+        }
+
+        /// <summary>
+        /// 문자열을 입력받아 괄호 쌍이 올바르게 짝지어져 있는지 검사합니다.
+        /// </summary>
+        /// <param name="str">괄호 쌍을 검사할 문자열</param>
+        /// <returns>괄호가 올바르게 짝지어져있다면 true</returns>
+        public static bool BracketPairCheck(string str)
+        {
+            Stack<char> brackets = new();
+            char pop = '\0';
+            
+            for (int i = 0; i < str.Length; i++)
+            {
+                switch (str[i])
+                {
+                    case '[':
+                    case '{':
+                    case '(':
+                        brackets.Push(str[i]);
+                        break;
+                    case ']':
+                    case '}':
+                    case ')':
+                        if (brackets.TryPop(out pop) == false)
+                            return false;
+                        else if (pop != GetBracketPair(str[i]))
+                            return false;
+                        break;
+                }
+            }
+            return true;
+        }
+
+
+        /// <summary>
+        /// 괄호에 해당하는 문자를 받아 짝을 이루는 괄호 문자를 반환합니다.<br/>
+        /// 매개변수로 받은 문자가 다음 목록에 해당하지 않으면 널 문자를 반환합니다.<br/>
+        /// []{}()&lt;&gt;
+        /// </summary>
+        /// <param name="c">괄호 짝을 찾을 문자</param>
+        /// <returns>괄호 짝</returns>
+        public static char GetBracketPair(char c)
+        {
+            switch(c)
+            {
+                case '[':
+                    return ']';
+                case ']':
+                    return '[';
+                case '{':
+                    return '}';
+                case '}':
+                    return '{';
+                case '(':
+                    return ')';
+                case ')':
+                    return '(';
+                case '<':
+                    return '>';
+                case '>':
+                    return '<';
+            }
+            return '\0';
+        }
+    }
+}
