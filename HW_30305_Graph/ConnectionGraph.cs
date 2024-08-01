ded0ef959b45536d13a7f143322ef49f7848703d
JeeSeong <rhtmxm17@gmail.com>
2024-08-01 22:35:05 +0900

    feat/Graph 과제 수행

---
+++ HW_30305_Graph/ConnectionGraph.cs
@@ -0,0 +1,59 @@
+﻿namespace HW_30305_Graph
+{
+    public abstract class ConnectionGraph
+    {
+        public abstract int Vertex { get; }
+        public abstract int Edge { get; }
+
+        public abstract void Connect(int from, int to);
+        public abstract void Disconnect(int from, int to);
+        public abstract bool GetConnection(int from, int to);
+        public abstract List<int> GetConnectionFrom(int from);
+    }
+
+    public class ListConnectionGraph : ConnectionGraph
+    {
+        // 행렬 대신 리스트 그래프를 사용한다는 점에서
+        // 간선의 조회와 삽입 삭제가 드물거나 평균적인 간선의 수가
+        // 적다고 가정하고 간선 정보를 담는데에 List를 사용
+        List<List<int>> graph;
+        int edge = 0;
+
+        public ListConnectionGraph(int vertex)
+        {
+            graph = new(vertex);
+            for (int i = 0; i < vertex; i++)
+            {
+                graph.Add(new List<int>());
+            }
+        }
+
+        public override int Vertex { get => graph.Count; }
+        public override int Edge { get => edge; }
+
+        public override void Connect(int from, int to)
+        {
+            if (false == graph[from].Contains(to))
+            {
+                graph[from].Add(to);
+                edge++;
+            }
+        }
+
+        public override void Disconnect(int from, int to)
+        {
+            if (graph[from].Remove(to))
+                edge--;
+        }
+
+        public override bool GetConnection(int from, int to)
+        {
+            return graph[from].Contains(to);
+        }
+
+        public override List<int> GetConnectionFrom(int from)
+        {
+            return new List<int>(graph[from]);
+        }
+    }
+}
---
+++ HW_30305_Graph/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace HW_30305_Graph
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionGraph graph = new ListConnectionGraph(8);
+
+            // 0 에서
+            graph.Connect(0, 1);
+
+            // 1 에서
+            graph.Connect(1, 0);
+            graph.Connect(1, 5);
+            graph.Connect(1, 6);
+
+            // 2 에서
+
+            // 3 에서
+            graph.Connect(3, 7);
+
+            // 4 에서
+
+            // 5 에서
+            graph.Connect(5, 3);
+            graph.Connect(5, 6);
+
+            // 6 에서
+            graph.Connect(6, 2);
+            graph.Connect(6, 4);
+            graph.Connect(6, 5);
+            graph.Connect(6, 7);
+
+            // 7 에서
+
+            // 그래프 읽기 및 출력
+            for (int i = 0; i < graph.Vertex; i++)
+            {
+                var connections = graph.GetConnectionFrom(i);
+
+                Console.WriteLine($"{i}번 정점:");
+
+                if (connections.Count == 0)
+                {
+                    Console.WriteLine("    (연결 없음)");
+                }
+
+                foreach (var connection in connections)
+                {
+                    Console.WriteLine($"    {connection}번 정점");
+                }
+            }
+        }
+    }
+}
